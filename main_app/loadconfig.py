469a12f3a072ea279c4949dc3c11af372e0f9d74
xzlrong233x <102354986+xzlrong233x@users.noreply.github.com>
2022-03-28 01:03:52 +0800

    Update logic

--- main_app/loadconfig.py
+++ main_app/loadconfig.py
@@ -1,4 +1,4 @@
-import json
+import json,os
 import win32api,win32con
 
 '''
@@ -21,10 +21,8 @@ CloseButtonClickY: 会议退出按钮的Y坐标
 CursorMoveWaitTime: 每次鼠标运动间隔的时间
 '''
 
-try:
-    with open(r'plan_config.json','r+',encoding='utf-8') as f:
-        PlanConfig = json.loads(f.read())
-except:
+
+if not os.path.exists(r'plan_config.json'):
     PlanConfig = {'DeBug': False,
     'LoadXlsxEnable': False, 
     'WebEnable': True, 
@@ -39,9 +37,11 @@ except:
     'CloseButtonClickX': 1010,
     'CloseButtonClickY': 590,
     'CursorMoveWaitTime': 15}
-    with open(r'plan_config.json','r+',encoding='utf-8') as f:
+    with open(r'plan_config.json','a+',encoding='utf-8') as f:
         f.write(json.dumps(PlanConfig))
     win32api.MessageBox(0,'加载配置文件出错,将使用默认配置\n如果你是第一次使用可以打开plan_config文件进行调试','注意',win32con.MB_OK)
+with open(r'plan_config.json','r+',encoding='utf-8') as f:
+    PlanConfig = json.loads(f.read())
 #test
 if __name__ == '__main__':
     print(PlanConfig)
