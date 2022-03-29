17449709797904d8459764b29cb25db51b26324c
xzlrong233x <102354986+xzlrong233x@users.noreply.github.com>
2022-03-29 17:05:41 +0800

    Updata info

--- main_app/main.py
+++ main_app/main.py
@@ -2,6 +2,7 @@
 #定时自动进入课堂，支持腾讯会议，腾讯课堂（作者上网课的平台就这些
 #运行源文件需pywin32 pyautogui helium selenium openpyxl win10toast库
 #author: xlr_233
+#from: https://github.com/xzlrong233x/auto-join-tencent-class-and-meet/
 from guimake import ControlGUI
 
 if __name__ == '__main__':
